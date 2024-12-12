1c1ed42acb2c52005f4a5ee465ac279889db98ad
natnael <nhattymack204@gmail.com>
2024-12-12 18:07:31 +0300

    feature(etsy): create listing redirect fix

--- src/app/customer/listings/page.tsx
+++ src/app/customer/listings/page.tsx
@@ -2,9 +2,12 @@
 
 import UserListingOptimization from "@/components/listings/UserListingOptimization";
 import { useAuth } from "@/contexts/AuthContext";
+import { useSearchParams } from "next/navigation";
 
 const ListingPage = () => {
   const { customerData } = useAuth();
+  const params = useSearchParams();
+  const code = params.get("code");
 
   return (
     <UserListingOptimization
@@ -12,6 +15,7 @@ const ListingPage = () => {
       showCreateListing={customerData?.isSuperCustomer}
       showDuplicatedListings={!customerData?.isSuperCustomer}
       showOptimizedListings={!customerData?.isSuperCustomer}
+      initialTab={code ? "create" : "optimized"}
     />
   );
 };
