e8d270353c71dca6f84322e1ccf60b2a8693eea6
“sheilagoopss” <sheila@goopss.com>
2024-12-12 15:57:56 +0800

    Re-add EtsyListing folder

---
+++ src/components/EtsyListing/EtsyListings.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { Spin } from "antd";
+import {
+  useEtsyListings,
+  useFetchOptimizedEtsyListings,
+} from "@/hooks/useEtsy";
+import { useEffect, useState, useCallback } from "react";
+import { IEtsyFetchedListing, IOptimizedEtsyListing } from "@/types/Etsy";
+import EtsyListingOptimizationList from "@/components/EtsyListing/Optimization/EtsyListingOptimizationList";
+
+interface EtsyListingsProps {
+  customerId: string;
+}
+
+const EtsyListings: React.FC<EtsyListingsProps> = ({ customerId }) => {
+  const { fetchEtsyListings, isFetchingEtsyListings } = useEtsyListings();
+  const { fetchOptimizedListing, isFetchingOptimizedListings } =
+    useFetchOptimizedEtsyListings();
+
+  const [etsyListings, setEtsyListings] = useState<
+    (IOptimizedEtsyListing | IEtsyFetchedListing)[]
+  >([]);
+
+  const refetch = useCallback(async () => {
+    const listings = await fetchEtsyListings({ customerId });
+    const optimizedListings = await fetchOptimizedListing();
+    const checkOptimized: (IOptimizedEtsyListing | IEtsyFetchedListing)[] =
+      listings.map((listing) => ({
+        ...listing,
+        id: optimizedListings.find(
+          (optimizedListing) =>
+            optimizedListing.listing_id === listing.listing_id,
+        )?.id,
+        isOptimized: optimizedListings.some(
+          (optimizedListing) =>
+            optimizedListing.listing_id === listing.listing_id,
+        ),
+        optimizedTitle:
+          optimizedListings.find(
+            (optimizedListing) =>
+              optimizedListing.listing_id === listing.listing_id,
+          )?.optimizedTitle || "",
+        optimizedDescription:
+          optimizedListings.find(
+            (optimizedListing) =>
+              optimizedListing.listing_id === listing.listing_id,
+          )?.optimizedDescription || "",
+        optimizedTags:
+          optimizedListings.find(
+            (optimizedListing) =>
+              optimizedListing.listing_id === listing.listing_id,
+          )?.optimizedTags || [],
+        optimizationStatus:
+          optimizedListings.find(
+            (optimizedListing) =>
+              optimizedListing.listing_id === listing.listing_id,
+          )?.optimizationStatus || false,
+        originalTitle: optimizedListings.find(
+          (optimizedListing) =>
+            optimizedListing.listing_id === listing.listing_id,
+        )?.title,
+        originalDescription: optimizedListings.find(
+          (optimizedListing) =>
+            optimizedListing.listing_id === listing.listing_id,
+        )?.description,
+        originalTags: optimizedListings.find(
+          (optimizedListing) =>
+            optimizedListing.listing_id === listing.listing_id,
+        )?.tags,
+      }));
+    setEtsyListings(checkOptimized);
+  }, [fetchEtsyListings, customerId, fetchOptimizedListing]);
+
+  useEffect(() => {
+    if (customerId) {
+      refetch();
+    }
+  }, [customerId, refetch]);
+
+  return (
+    <>
+      {isFetchingEtsyListings || isFetchingOptimizedListings ? (
+        <div
+          style={{
+            width: "100%",
+            minHeight: "30vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Spin />
+        </div>
+      ) : (
+        <EtsyListingOptimizationList
+          listings={etsyListings}
+          selectedCustomerId={customerId}
+          refetch={refetch}
+        />
+      )}
+    </>
+  );
+};
+
+export default EtsyListings;
